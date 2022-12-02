b8136768b11d24ad8a318a8fe7bbc380e9e5b2ac
TechnoSavage <technosavage@protonmail.com>
2022-12-02 09:34:34 -0500

    solutions for Advent of Code puzzles

---
+++ 2022/AoC_2022_1.py
@@ -0,0 +1,20 @@
+calories = []
+with open('AoC_2022_1.txt', 'r') as food:
+    for line in food:
+        calories.append(line)
+elves = {}
+tally = 1
+pack = []
+for item in calories:
+    if item != '\n':
+        pack.append(int(item.strip('\n')))
+    else:
+        elves[tally] = pack
+        pack = []
+        tally += 1
+totals = []
+for item in elves:
+    total = sum(elves[item])
+    totals.append(total)
+totals.sort()
+print(totals[-1])
