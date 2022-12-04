11576d4b1d9fc3187e07d3dc4e0fc72fb2dbfc58
TechnoSavage <technosavage@protonmail.com>
2022-12-04 18:16:59 -0500

    Solutions to Advent of Code challenges.

---
+++ 2022/AoC_2022_4.py
@@ -0,0 +1,32 @@
+def makeLists(start, end):
+    start, end = int(start), int(end)
+    if start != end:
+        myList = list(range(start, end + 1))
+    else:
+        myList = [start]
+    return myList
+
+pairs = []
+overlapping = 0
+with open('AoC_2022_4.txt', 'r') as assignments:
+    for pair in assignments:
+        pairs.append(pair.strip('\n'))
+for pair in pairs:
+    div = pair.split(',')
+    elf1, elf2 = div[0], div[1]
+    elf1, elf2 = elf1.split('-'), elf2.split('-')
+    elf1 = makeLists(elf1[0], elf1[1])
+    elf2 = makeLists(elf2[0], elf2[1])
+    if(set(elf1).issubset(set(elf2))):
+        overlapping += 1
+    elif(set(elf2).issubset(set(elf1))):
+        overlapping += 1
+    else:
+        pass
+    # if elf1[0] in elf2 and elf1[-1] in elf2:
+    #     overlapping += 1
+    # elif elf2[0] in elf1 and elf2[-1] in elf1:
+    #     overlapping += 1
+    # else:
+        #pass
+print(overlapping)
