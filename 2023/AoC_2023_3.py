205f3ccaf4eb1bd852881e18f61f4180d3535a8f
TechnoSavage <technosavage@protonmail.com>
2023-12-05 18:55:33 -0500

    .

--- 2023/AoC_2023_3.py
+++ 2023/AoC_2023_3.py
@@ -1,68 +1,3 @@
-import re
-from pprint import pprint
-
-#Misunderstood the problem and started finding all numbers following a part symbol
-# total = 0
-# partNumbers = []
-# with open ('AoC_2023_3.txt', 'r') as f:
-#     schematic = f.read().replace('\n', '')
-#     parts = re.findall("([\+\-\*\@\=\$\/\%\&\#]\.*[0-9]+)", schematic)
-#     print(parts)
-#     for part in parts:
-#         pNo = re.findall("[0-9]+", part)
-#         partNumbers.append(int(pNo[0]))
-# print(sum(partNumbers))
-
-# partNos = []
-# partChars = ('+', '-', '*', '@', '=', '$', '%', '&', '#')
-# with open ('AoC_2023_3.txt', 'r') as f:
-#     schematic = f.read().replace('\n', '')
-#     #140 lines
-#     #140 characters per line
-#     loc = 0
-#     for char in schematic:
-#         quadrant = []
-#         if char in partChars:
-#             quadrant.append(schematic[loc - 143: loc -136])
-#             quadrant.append(schematic[loc - 3: loc + 4])
-#             quadrant.append(schematic[loc + 137: loc + 144])
-#         loc += 1
-#         if len(quadrant) > 0:
-#             if quadrant[0][3] == '.':
-#                 tleft = quadrant[0][0:4]
-#                 tright = quadrant[0][3:7]
-#                 match1 = re.match("([1-9]{1}[0-9]{1,2})\.", tleft)
-#                 match2 = re.match("\.([1-9]{1}[0-9]{1,2})", tright)
-#                 if match1 is not None:
-#                     partNos.append(match1[1])
-#                 if match2 is not None:
-#                     partNos.append(match2[1])
-#             else:
-#                  topMatch = re.search("\.{1,3}([0-9]{0,3})\.{1,3}", quadrant[0])
-#                  if topMatch is not None:
-#                      partNos.append(topMatch[1])
-#             midMatch = re.match("(\.?[0-9]+)[\+\-\*\@\=\$\/\%\&\#]([0-9]+)", quadrant[1])
-#             if midMatch is not None:
-#                 partNos.append(midMatch[1])
-#                 partNos.append(midMatch[2])
-#             if quadrant[2][3] == '.':
-#                 bleft = quadrant[2][0:3]
-#                 bright = quadrant[2][3:6]
-#                 match3 = re.match("([1-9]{1}[0-9]{1,2})\.", bleft)
-#                 match4 = re.match("\.([1-9]{1}[0-9]{1,2})", bright)
-#                 if match3 is not None:
-#                     partNos.append(match3[1])
-#                 if match4 is not None:
-#                     partNos.append(match4[1])
-#             else:
-#                  bottomMatch = re.search("\.{1,3}([0-9]{0,3})\.{1,3}", quadrant[2])
-#                  if bottomMatch is not None:
-#                      partNos.append(bottomMatch[1])
-# partNos = [int(i.replace('.', '')) for i in partNos]           
-# # partNos = [int(i) for i in partNos if i != '']
-# #print(partNos)
-# print(sum(partNos))
-
 #Ugly but it works
 def seek(col, row, number, partChars, schematic):
     numSize = len(str(number))
@@ -106,7 +41,6 @@ if __name__ == "__main__":
     with open ('AoC_2023_3.txt', 'r') as f:
         schematic = f.read()
     schematic = [list(line) for line in schematic.split('\n')]
-    #pprint(schematic)
     col = 0
     row = 0
     buffer = []
