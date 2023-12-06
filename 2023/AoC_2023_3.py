26447529b72d8ff1117f0e557823eb01a5520f82
TechnoSavage <technosavage@protonmail.com>
2023-12-05 22:43:40 -0500

    .

--- 2023/AoC_2023_3.py
+++ 2023/AoC_2023_3.py
@@ -1,62 +1,61 @@
-#Ugly but it works
-def seek(col, row, number, partChars, schematic):
-    numSize = len(str(number))
-    if schematic[col][row] in partChars:
-        return True
-    if row - numSize - 1 >= 0: 
-        if schematic[col][row - numSize - 1] in partChars:
-            return True
-    if col - 1 >= 0:
-        if schematic[col - 1][row] in partChars:
-            return True
-        if schematic[col - 1][row - 1] in partChars:
-            return True
-        if schematic[col - 1][row - 2] in partChars:
-            return True
-        if numSize >= 2:
-            if schematic[col - 1][row - 3] in partChars:
+import pandas as pd
+
+def partScan(targets, schematic):
+    parts = ('+', '-', '*', '@', '=', '$', '%', '&', '#', '/')
+    adjacent = [(0, -1), (0, +1),
+                (+1, -1), (+1, 0), (+1, +1),
+                (-1, -1), (-1, 0), (-1, +1)]
+    for target in targets:
+        for mod in adjacent:
+            try:
+                char = schematic.iloc[target[0] + mod[0], target[1] + mod[1]]
+            except IndexError as e:
+                pass
+            if char in parts:
                 return True
-        if numSize == 3:
-            if schematic[col - 1][row - 4] in partChars:
-                return True
-    if col + 1 <= 139:
-        if schematic[col + 1][row] in partChars:
-            return True
-        if schematic[col + 1][row - 1] in partChars:
-            return True
-        if schematic[col + 1][row - 2] in partChars:
-            return True
-        if numSize >= 2:
-            if schematic[col + 1][row - 3] in partChars:
-                return True
-        if numSize == 3:
-            if schematic[col + 1][row - 4] in partChars:
-                return True
-    return False
+    return False        
+    
+def buildNum(targets, schematic):
+    number = []
+    for target in targets:
+        digit = schematic.iloc[target[0], target[1]]
+        number.append(digit)
+    return ''.join(number)
+
+
+def main():
+    partNums = []
+    with open ('AoC_2023_3.txt', 'r') as f:
+        draft = f.read()
+    blueprint = [list(line) for line in draft.split('\n')]
+    schematic = pd.DataFrame(blueprint)
+    print(schematic)
+    yLimit = schematic.shape[0]
+    xLimit = schematic.shape[1]
+    x = 0
+    y = 0
+    while y < yLimit:
+        targets = []
+        while x < xLimit:
+            if str.isdigit(schematic.iloc[y, x]):
+                targets.append((y, x))
+            elif x == xLimit - 1 and len(targets) > 0:
+                test = partScan(targets, schematic)
+                if test:
+                    partNums.append(buildNum(targets, schematic))
+                targets = []
+            else:
+                if len(targets) > 0:
+                    test = partScan(targets, schematic)
+                    if test:
+                        partNums.append(buildNum(targets, schematic))
+                targets = []
+            x += 1
+        x = 0
+        y += 1
+    partNums = [int(i) for i in partNums]
+    print(sum(partNums))
 
 if __name__ == "__main__":
-    partChars = ('+', '-', '*', '@', '=', '$', '%', '&', '#')
-    partNos = []
-    with open ('AoC_2023_3.txt', 'r') as f:
-        schematic = f.read()
-    schematic = [list(line) for line in schematic.split('\n')]
-    col = 0
-    row = 0
-    buffer = []
-    while col <= 139:
-        cursor = schematic[col][row]
-        if str.isdigit(cursor):
-            buffer.append(cursor)
-        else:
-            if len(buffer) > 0:
-                number = ''.join(buffer)
-                part = seek(col, row, number, partChars, schematic)
-                if part == True:
-                    partNos.append(number)
-                buffer = []
-        row += 1
-        if row > 139:
-            col += 1
-            row = 0
-    partNos = [int(i) for i in partNos]
-    print(sum(partNos))
+    main()
+    
